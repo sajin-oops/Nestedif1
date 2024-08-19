00e80d35b86a1ec4d1111ec3c3497e8e556d6233
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-19 19:30:49 +0530

    Add files via upload

---
+++ nestedif1.py
@@ -0,0 +1,15 @@
+x = 11
+
+if x >10:
+  print("Above ten,")
+  if x > 20:
+    print("And also above 20!")
+  else:
+    print("But not above 20")
+    
+
+# O/P 
+'''
+Above ten,
+But not above 20
+'''
